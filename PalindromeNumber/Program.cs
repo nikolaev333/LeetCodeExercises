f549cb71fd33364c41abe7e54ad3fad879635f7d
Andrey Nikolaev <andrei_nikolaev@abv.bg>
2024-03-20 17:05:23 +0200

    Palindrome Number

---
+++ PalindromeNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class IsPalindrome
+{
+    public static void Main(string[] args)
+    {
+        bool result = IsPalindrome1(121);
+        bool result1 = IsPalindrome1(-121);
+        bool result2 = IsPalindrome1(123);
+        
+        Console.WriteLine(result); //True
+        Console.WriteLine(result1); //False
+        Console.WriteLine(result2); //False
+    }
+
+    public static bool IsPalindrome1(int x)
+    {
+        if (x < 0) return false;
+
+        int temp = x;
+        int reverse = 0;
+
+        while (temp != 0)
+        {
+            int digit = temp % 10;
+            reverse = reverse * 10 + digit;
+            temp /= 10;
+        }
+
+        return reverse == x;
+    }
+}
