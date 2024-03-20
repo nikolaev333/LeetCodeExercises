9f8ea36eed859f6e4fa40db0e8c7f96186e76cfe
Andrey Nikolaev <andrei_nikolaev@abv.bg>
2024-03-20 17:12:57 +0200

    Two Sum exercise was added

---
+++ TwoSum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Solution {
+    public static void Main(string[] args) {
+        // Създаваме инстанция на Solution
+        var solution = new Solution();
+        
+        // Дефинираме тестов масив и целева стойност
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        
+        // Извикваме метода TwoSum и печатаме резултата
+        int[] result = solution.TwoSum(nums, target);
+        if (result != null) {
+            Console.WriteLine($"Indices: {result[0]}, {result[1]}");
+        } else {
+            Console.WriteLine("No two sum solution found.");
+        }
+    }
+    
+    public int[] TwoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.Length; i++) {
+            for (int j = i + 1; j < nums.Length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return null; // Връщаме null ако решение не бъде намерено
+    }
+}
