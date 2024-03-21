b3972c2843b2c71817c586d30e74af5760d6722d
Andrey Nikolaev <andrei_nikolaev@abv.bg>
2024-03-21 12:22:02 +0200

    Majority Element exercise was updated

---
+++ MajorityElement/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace MajorityElement;
+
+public class MajorityElementEx
+{
+    public static void Main(string[] args)
+    {
+        int[] nums = { 2, 2, 1, 1, 1, 2, 2 };
+        Console.WriteLine("The majority element is: " + FindMajorityElement(nums));
+    }
+
+    private static int FindMajorityElement(int[] nums)
+    {
+        int count = 0;
+        int candidate = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (count == 0)
+            {
+                candidate = nums[i];
+            }
+
+            count += (nums[i] == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+}
